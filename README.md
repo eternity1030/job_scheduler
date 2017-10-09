259622b82d14e127c69945a72641d6e60b988390
NITI SHARMA <shadowfax1j@NITIs-MacBook-Pro.local>
2017-10-08 17:39:20 -0700

    Added the ReadMe and minor tweaks.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# job_scheduler
+# job-scheduler
+
+Details
+
+  - Keeps track of jobs to be executed in future
+  - Input for the script is an Array of Jobs like:
+  ```
+  [(job_1, delay_1), (job_2,delay_2) ..... ]
+  ```
+  - Output: Prints the jobId executed and the time it got executed.
+  - Performance Metrics: Should be able to support 1000s of jobs.
+
+# prerequisites
+Install Python 2.7 type following command in the terminal window
+  ```
+  brew install python
+  ```
+  If you don't have homebrew, type following in the terminal window
+  ```
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ```
+
+### Running the Script
+After cloning the repository, go to the root and execute the following:
+```
+python scheduler.py
+```
+
+##### Input Format
+At any time, you can add the jobs with following format
+  ```
+  [(job_1, delay_1), (job_2,delay_2) ..... ]
+  EX:
+  >> [(1,0), (2,3),(3,4)]
+  ... jobs will get excuted ..
+  [(4,6)]
+  ```
+
+![alt text](/Users/shadowfax1j/Documents/Reading_int/job_scheduler)
--- scheduler.py
+++ scheduler.py
@@ -1,28 +1,44 @@
 # ****** REQUIREMENTS/NOTES ******
 # queue keeping track of jobs to be executed in future ....
 # Input : job id and time_delay
-# output: printing job_id and current time
+# output: printing job_id and current time ...
+# Performance Metrics: should be able to support 1000s of jobs.
+
+# ******* Approach Taken ***********
+# > Maintaining a Hash where key is the timeStamp and value is an Array of jobs to be run at that particular time.
+# > Created a timer that will look for Hash Key corresponding to current time O(1) and if Key found then will execute the jobs and then delete the key.
+# ******* Things to Avoid *******
+# > Looping through the given data structure, in order to maintain time & space complexity.
+# > Not having too many Threads.
+
+
 
 from datetime import datetime, timedelta
 import time
 import threading
-tmr = None
+tmr = None # keeping track of gobal timer obj.
+
 
 class Scheduler(object):
     def __init__(self):
-        self.scheduled_jobs = {}
+        self.scheduled_jobs = {} # Hash for storing Key=timeStamp and Value=[jobs]
 
     def schedule_job(self, job):
         self.add_task(job)
 
     def add_task(self, job):
+        # ex: If the Job is (1,0) then the timeStamp Key will be nothing but current time.
+        #       If Job is (2,5) then the timeStamo key = current_time + 5sec delay
         job_execution_key = (datetime.now() + timedelta(seconds=job[1])).strftime("%Y-%m-%d %H:%M:%S")
 
+        # If there is already jobs scheduled to run for the above timeStamp Key then simply append the job to
+        #   exiting list of pending Jobs.
         if job_execution_key in self.scheduled_jobs:
             job_lst = self.scheduled_jobs[job_execution_key]
             job_lst.append(job[0])
             self.scheduled_jobs[job_execution_key] = job_lst
         else:
+            # Create a new entry and add job to be executed in time "x"
             self.scheduled_jobs[job_execution_key] = [job[0]]
 
         self.log_msg("Job:",  job[0], "added at: ", self.date_time())
@@ -32,10 +48,13 @@ class Scheduler(object):
         tmr = threading.Timer(1,self.run)
         tmr.start()
 
+        # At any given amount of time check for the timeStamp Key in the Hash.
+        # If found then fetch the list of jobs and execute in the same order.
+        # And once executed delete the Key.
         current_time = self.date_time()
         if current_time in self.scheduled_jobs:
             jobs_to_run = self.scheduled_jobs[current_time]
-            self.log_msg("Ran Job: ", jobs_to_run, "at", current_time, "\n")
+            self.log_msg("Ran Job(s): ", jobs_to_run, "at", current_time)
 
             try:
                 del self.scheduled_jobs[current_time]
@@ -48,6 +67,7 @@ class Scheduler(object):
     def log_msg(self, *msg):
         print msg
 
+
 message = "Please enter the jobs in following format: \n \
 [(x,y)] => where 'X' is the job and 'Y' is the delay. \n \
 ****************************************** \n \
@@ -56,7 +76,7 @@ To EXIT, please enter '0'  \n \
 print message
 
 job_schedular = Scheduler()
-job_schedular.run()
+job_schedular.run()     # Start the timer ....
 
 while(True):
     user_input = input(">> ")
